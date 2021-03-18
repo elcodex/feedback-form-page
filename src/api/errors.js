b6dc744f58582a507602d5bfc8ef4f1f8bf20866
Elen <elcrses@ya.ru>
2021-03-18 18:17:03 +0300

    errors: fix typos

--- src/api/errors.js
+++ src/api/errors.js
@@ -1,6 +1,6 @@
 export const ERRORS = {
     SERVER: "Server error. Please try again later.",
-    FILE_SIZE: "File size is more than 2 Mb.",
+    FILE_SIZE: "Files size is more than 2 mb.",
     FILE_EMPTY: "File is empty.",
     GLITCH: "Well, you found a glitch."
 }
